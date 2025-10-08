4575ddd30ee2e5af88f2de34e78acf1557ffb434
Olena Mustafina <toysamiy.mail@gmail.com>
2025-10-08 12:23:49 +0300

    feat: Update characters.js: Add animation effects buttons group and add loadModal and default value for rendering page

--- src/js/characters.js
+++ src/js/characters.js
@@ -1,16 +1,24 @@
 import { createCharacterCard } from './CharacterCard.js';
+import { showCharacterModal } from './characterModal.js';
 
 const container = document.getElementById('charactersContainer');
 const buttons = document.querySelectorAll('[data-house]');
 let characters = [];
 
+async function loadModal() {
+    const res = await fetch('../components/CharacterModal.html');
+    const html = await res.text();
+    document.body.insertAdjacentHTML('beforeend', html);
+}
+
+await loadModal(); 
 
 async function loadCharacters() {
     try {
         const res = await fetch('https://hp-api.onrender.com/api/characters');
         characters = await res.json();
 
-        renderCharacters(characters); 
+        renderCharacters('Gryffindor'); 
     }  catch (err) {
     console.error('Error:', err);
   }   
@@ -21,13 +29,31 @@ function renderCharacters(house) {
     const filtered = characters.filter(c => c.house === house);
 
     filtered.forEach(character => {
-        container.innerHTML += createCharacterCard(character);
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = createCharacterCard(character); 
+        container.appendChild(wrapper.firstElementChild);
       });
+
+    document.querySelectorAll('.show-more-btn').forEach(btn => {
+        btn.addEventListener('click', () => {
+            const data = JSON.parse(btn.getAttribute('data-character'));
+            showCharacterModal(data);
+        });
+    });
 }
 
 buttons.forEach(btn => {
-    btn.addEventListener('click', () => renderCharacters(btn.dataset.house));
-});
+    btn.addEventListener('click', () => {
+        buttons.forEach(btn => {
+            btn.classList.remove("btn-warning", "text-dark", "fw-bold", "active");
+            btn.classList.add("btn-transparent", "text-white");
+        });
 
+        btn.classList.remove("btn-transparent", "text-white");
+        btn.classList.add("btn-warning", "text-dark", "fw-bold", "active");
+
+        renderCharacters(btn.dataset.house);
+    });
+});
 
 loadCharacters();
