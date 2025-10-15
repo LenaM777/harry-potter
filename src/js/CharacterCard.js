42fb284ae25a46898364a3d02dca1a9421f8e50f
Olena Mustafina <toysamiy.mail@gmail.com>
2025-10-15 11:25:38 +0300

    chore: remove unused files and refine global Bootstrap styles

--- src/js/CharacterCard.js
+++ src/js/CharacterCard.js
@@ -3,11 +3,11 @@ export function createCharacterCard(character) {
     <div class="col-12 col-sm-6 col-md-4 col-lg-3">
         <div class="card card-character bg-dark text-white rounded-4 border-0 glow h-100">
             <img src="${character.image || '/public/assets/images/emblem.jpg'}" class="card-img" alt="Character">
-            <div class="card-img-overlay d-flex align-items-end p-4 gradient-overlay">
+            <div class="card-img-overlay d-flex align-items-end p-3 p-md-4 gradient-overlay">
                 <div>
-                    <h3 class="card-title">${character.name}</h3>
+                    <h4 class="card-title">${character.name}</h4>
                     <p class="card-text text-warning mb-1"> ${character.house || 'Hogwarts'}</p>
-                    <p class="card-text text-warning mb-3"> ${character.dateOfBirth || 'Unknown'}</p>
+                    <p class="card-text text-warning mb-2"> ${character.dateOfBirth || 'Unknown'}</p>
                     <button class="btn bg-transparent text-white border-0 p-0 d-inline-flex align-items-center gap-2 show-more-btn" data-character='${JSON.stringify(character)}'>
                     More info<span class="arrow">&rarr;</span>
                     </button>
